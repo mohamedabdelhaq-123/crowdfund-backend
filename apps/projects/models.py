9c47cddcef9a3c2684c54b25de42b13c8a23fcd8
Andrew emad <andrew14emad@gmail.com>
2026-04-15 03:55:52 +0200

    Revert "Feat/home page categories"

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,18 +1,3 @@
 from django.db import models
-from django.conf import settings
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name if self.name else f"Category {self.id}"
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name if self.name else f"Tag {self.id}"
+# Create your models here.
