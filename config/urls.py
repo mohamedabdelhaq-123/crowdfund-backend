bb590e6df8e1edb7155c2d2d1c7e838692961cd1
Mohamed Sameh <mohamed.kholy2011@gmail.com>
2026-04-16 03:31:27 +0200

    Update

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.tag.urls')),
     path('auth/', include('apps.authentication.urls')),
 
     path('api/users/', include('apps.profiles.urls')),
